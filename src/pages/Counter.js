638d8b8ed104fd044d95694e775832598543fdfb
Jinhwan27 <jinhwan.byun@mdstech.co.kr>
2023-04-03 16:56:25 +0900

    40:22

---
+++ src/pages/Counter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Counter = () => {
+    return (
+        <div>
+            <button>+1</button>
+            <button>-1</button>
+            <p></p>
+        </div>
+    )
+}
