227c90e417d35a8adf183ec36b13365e56feda00
Jinhwan27 <jinhwan.byun@mdstech.co.kr>
2023-04-03 16:50:37 +0900

    한시간 만에 끝내는 React.js 입문 37:07

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Routes, Route, Link} from 'react-router-dom';
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
+    // 자바스크립트 안에서 XML 문법을 사용할 수 있게 하는 JSX 임 완벽하게 HTML과 같지 않음 @주의@
+    // 리액트는 싱글페이지 어플리케이션임 INDEX.HTML 하나만 쓰고 DIV에 페이지이동마다 해당되는 애들이 들어감 그래서 개빠름 서버사이트렌더링이 아님 클라이언트단에서 처리됨
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+      <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+      </Routes>
+      <Home/>
+
     </div>
   );
 }
