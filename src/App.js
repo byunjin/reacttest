99a9456172a3ede529380bb38e37d61267289b43
byunjin <byon1228@gmail.com>
2023-04-03 22:35:43 +0900

    학습종료

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Routes, Route, Link} from 'react-router-dom';
+import About from './pages/About';
+import Home from "./pages/Home";
+import Counter from './pages/Counter';
+import Input from './pages/Input';
+import Input2 from './pages/Input2';
+import UserList from './pages/List';
+
 
 function App() {
+  //jsx여서 완벽하게 html과 같지않으니깐 주의
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> | 
+        <Link to="/counter">Counter</Link> | <Link to="/input">Input</Link> | 
+         <Link to="/input2">Input2</Link> | <Link to="/list">UserList</Link>
+
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/counter' element={<Counter/>}></Route>
+        <Route path='/input' element={<Input/>}></Route>
+        <Route path='/input2' element={<Input2/>}></Route>
+        <Route path='/list' element={<UserList/>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+//함수형
+import React from "react";
+
+const  Home = () => {
+    return <h1>Home 화면입니다.</h1>
+}
+
+export default Home;
+
+// class 형
+// import React, {Component} from "react";
+
+// class Home extends Component {
+//     render(){
+//         return <h1>Home 화면입니다.</h1>
+//     }
+// }
+
+// export default Home;
